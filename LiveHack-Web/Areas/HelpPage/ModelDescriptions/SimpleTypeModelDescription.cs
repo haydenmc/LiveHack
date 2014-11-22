ebfc3b451455216776f04d8d620cf80db10551da
Hayden <hayden@outlook.com>
2014-11-22 14:01:33 -0500

    Initial commit.

---
+++ LiveHack-Web/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace LiveHack_Web.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ LiveHack-Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LiveHack_Web.MvcApplication" Language="C#" %>
