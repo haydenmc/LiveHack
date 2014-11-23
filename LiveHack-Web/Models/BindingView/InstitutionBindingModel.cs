831075cf149edcad78fa9f60576c557b33c12a74
lucaslou <lucaslou317@gmail.com>
2014-11-23 01:02:36 -0500

    binding models fixed

--- LiveHack-Web/Models/BindingView/InstitutionBindingModel.cs
+++ LiveHack-Web/Models/BindingView/InstitutionBindingModel.cs
@@ -8,19 +8,11 @@ namespace LiveHack_Web.Models.Viewmodels
 {
     public class InstitutionBindingModel
     {
-        public InstitutionBindingModel(Institution ins)
-		{
-            this.InstitutionId  = ins.InstitutionId;
-            this.Name           = ins.Name;
-            this.ZipCode        = ins.ZipCode;
-			this.GroupId		= ins.Group.GroupId;
-			//this.Group			= new InstitutionGroupViewModel(ins.Group);
-            //this.Users			= ins.Users.Select(x => new UserViewModel(x)).ToList();
-		}
-        public Guid InstitutionId { get; set; }
+        
+       // public Guid InstitutionId { get; set; }
         public string Name { get; set; }
         public string ZipCode { get; set; }
-		public Guid GroupId { get; set; }
+		//public Guid GroupId { get; set; }
 		//public InstitutionGroupViewModel Group { get; set; }
 		//public ICollection<UserViewModel> Users { get; set; }
 	}
--- LiveHack-Web/Models/BindingView/UserBindingModel.cs
+++ LiveHack-Web/Models/BindingView/UserBindingModel.cs
@@ -8,18 +8,9 @@ namespace LiveHack_Web.Models.Viewmodels
 {
     public class UserBindingModel
     {
-        public UserBindingModel(User user)
-        {
-            this.Id				= user.Id;
-            this.Email			= user.Email;
-			this.HackathonIds	= user.Hackathons.Select(x => x.HackathonId).ToList();
-			this.InstitutionId	= user.Institution.InstitutionId;
-            //this.Hackathons   = user.Hackathons.Select(x => new HackathonViewModel(x)).ToList();
-            //this.Institution	= new InstitutionViewModel(user.Institution);
 
-        }
-		public ICollection<Guid> HackathonIds { get; set; }
-		public Guid InstitutionId { get; set; }
+		//public ICollection<Guid> HackathonIds { get; set; }
+		//public Guid InstitutionId { get; set; }
         //public ICollection<HackathonViewModel> Hackathons { get; set; }
         //public InstitutionViewModel Institution { get; set; }
         public string Id { get; set; }
