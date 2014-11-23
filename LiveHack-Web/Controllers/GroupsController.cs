55a1b28cf30b5697f3b233fc8f5296dffcc0a7b8
Adam Bahrainwala <abahrain@purdue.edu>
2014-11-22 22:41:25 -0500

    Group controller

---
+++ LiveHack-Web/Controllers/GroupsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using LiveHackDb;
+using LiveHackDb.Models;
+
+namespace LiveHack_Web.Controllers
+{
+    public class GroupsController : ApiController
+    {
+        private LiveHackDbContext db = new LiveHackDbContext();
+
+        // GET: api/Groups
+        public IQueryable<Group> GetGroups()
+        {
+            return db.Groups;
+        }
+
+        // GET: api/Groups/5
+        [ResponseType(typeof(Group))]
+        public IHttpActionResult GetGroup(Guid id)
+        {
+            Group group = db.Groups.Find(id);
+            if (group == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(group);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool GroupExists(Guid id)
+        {
+            return db.Groups.Count(e => e.GroupId == id) > 0;
+        }
+    }
+}
