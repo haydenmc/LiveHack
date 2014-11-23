412065bbfa6548e21f0fb92b38a4aaeeec908e98
Darren Cheng <zades@outlook.com>
2014-11-22 20:58:32 -0500

    Some skeleton code

---
+++ LiveHack-Web/Models/Viewmodels/HackathonViewModel.cs
@@ -0,0 +1,18 @@
+﻿using LiveHackDb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LiveHack_Web.Models.Viewmodels
+{
+	public class HackathonViewModel
+	{
+		public HackathonViewModel(Hackathon hack)
+		{
+			
+		}
+
+		public Guid HackathonId { get; set; }
+	}
+}
