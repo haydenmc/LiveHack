9005299080c14fcae5953a09c78a4f3717a6efc9
lucaslou <lucaslou317@gmail.com>
2014-11-23 01:10:40 -0500

    fix some bugs from nowhere

--- LiveHack-Web/Models/Viewmodels/GroupViewModel.cs
+++ LiveHack-Web/Models/Viewmodels/GroupViewModel.cs
@@ -11,20 +11,20 @@ namespace LiveHack_Web.Models.Viewmodels
 		public GroupViewModel(Group group)
 		{
 			this.GroupId		= group.GroupId;
-			this.Members		= group.Members.Select(x => new UserBindingModel(x)).ToList();
-			this.Guests			= group.Guests.Select(x => new UserBindingModel(x)).ToList();
+			this.Members		= group.Members.Select(x => new UserViewModel(x)).ToList();
+			this.Guests			= group.Guests.Select(x => new UserViewModel(x)).ToList();
 			this.Name			= group.Name;
 			this.Description	= group.Description;
 			this.Url			= group.Url;
-			this.Messages		= group.Messages.Select(x => new MessageBindingModel(x)).ToList();
+			this.Messages		= group.Messages.Select(x => new MessageViewModel(x)).ToList();
 		}
 
 		public Guid GroupId { get; set; }
-		public ICollection<UserBindingModel> Members { get; set; }
-		public ICollection<UserBindingModel> Guests { get; set; }
+		public ICollection<UserViewModel> Members { get; set; }
+		public ICollection<UserViewModel> Guests { get; set; }
 		public string Name { get; set; }
 		public string Description { get; set; }
 		public string Url { get; set; }
-		public ICollection<MessageBindingModel> Messages { get; set; }
+		public ICollection<MessageViewModel> Messages { get; set; }
 	}
 }
