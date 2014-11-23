c0c317f1379e7892c11e43aef2c28c48220d92a7
lucaslou <lucaslou317@gmail.com>
2014-11-22 23:40:26 -0500

    team group controller changes

--- LiveHack-Web/Controllers/TeamGroupsController.cs
+++ LiveHack-Web/Controllers/TeamGroupsController.cs
@@ -10,6 +10,8 @@ using System.Web.Http;
 using System.Web.Http.Description;
 using LiveHackDb;
 using LiveHackDb.Models;
+using LiveHack_Web;
+using LiveHack_Web.Models.Viewmodels;
 
 namespace LiveHack_Web.Controllers
 {
@@ -18,22 +20,23 @@ namespace LiveHack_Web.Controllers
         private LiveHackDbContext db = new LiveHackDbContext();
 
         // GET: api/TeamGroups
-        public IQueryable<TeamGroup> GetGroups()
+        public IQueryable<TeamGroupViewModel> GetGroups()
         {
-            return db.Groups.OfType<TeamGroup>();
+            return db.Groups.OfType<TeamGroup>().Select(x => new TeamGroupViewModel(x));
         }
 
         // GET: api/TeamGroups/5
+        [Route("{id}")]
         [ResponseType(typeof(TeamGroup))]
         public IHttpActionResult GetTeamGroup(Guid id)
         {
-            TeamGroup teamGroup = db.Groups.OfType<TeamGroup>().Where(x => x.GroupId == id).FirstOrDefault();
-            if (teamGroup == null)
+            TeamGroupViewModel ttest = new TeamGroupViewModel(db.Groups.OfType<TeamGroup>().Where(x => x.GroupId == id).FirstOrDefault());
+            if (ttest == null)
             {
                 return NotFound();
             }
 
-            return Ok(teamGroup);
+            return Ok(ttest);
         }
 
         protected override void Dispose(bool disposing)
