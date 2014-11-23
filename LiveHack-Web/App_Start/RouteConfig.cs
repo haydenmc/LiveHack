20ba23dcc3c9ceab561a35151aae5ae2865469e0
Hayden <hayden@outlook.com>
2014-11-23 02:06:32 -0500

    Hey look some log in thing

--- LiveHack-Web/App_Start/RouteConfig.cs
+++ LiveHack-Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace LiveHack_Web
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
-				defaults: new { controller = "Hackathon", action = "Index", id = UrlParameter.Optional }
+				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
 			);
 		}
 	}
