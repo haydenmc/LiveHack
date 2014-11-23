960b1c675fc401f617b218a66cae67cecf135925
Hayden <hayden@outlook.com>
2014-11-23 07:19:19 -0500

    Added a seed method and a bunch of stuff.

--- LiveHackDb/Migrations/Configuration.cs
+++ LiveHackDb/Migrations/Configuration.cs
@@ -1,9 +1,11 @@
 namespace LiveHackDb.Migrations
 {
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
+	using LiveHackDb.Models;
+	using System;
+	using System.Collections.Generic;
+	using System.Data.Entity;
+	using System.Data.Entity.Migrations;
+	using System.Linq;
 
     public sealed class Configuration : DbMigrationsConfiguration<LiveHackDb.LiveHackDbContext>
     {
@@ -26,6 +28,35 @@ namespace LiveHackDb.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+
+			context.Hackathons.Add(new Hackathon()
+			{
+				HackathonId = Guid.NewGuid(),
+				Name = "Wildhacks 2014",
+				ShortName = "wildhacks",
+				Description="Wildhacks 2014 yay hooray",
+				Groups = new List<HackathonGroup>()
+				{
+					new HackathonGroup() {
+						GroupId = Guid.NewGuid(),
+						Description = "Main hackathon group",
+						Name = "Main"
+					}
+				},
+				StartDateTime= DateTimeOffset.Now,
+				EndDateTime=DateTimeOffset.Now.Add(TimeSpan.FromDays(2)),
+				Institution = new Institution()
+				{
+					InstitutionId=Guid.NewGuid(),
+					Name="Northwestern University",
+					Group = new InstitutionGroup()
+					{
+						GroupId=Guid.NewGuid(),
+						Description="Group for Northwestern University",
+						Name="Northwestern University Group"
+					}
+				}
+			});
         }
     }
 }
