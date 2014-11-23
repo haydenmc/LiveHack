b2252dd6404940fc1323488ac75e936d107a1a67
Darren Cheng <zades@outlook.com>
2014-11-22 22:21:23 -0500

    Fixed a few things with sponsorgroupviewmodel

---
+++ LiveHack-Web/Models/Viewmodels/SponsorGroupViewModel.cs
@@ -0,0 +1,16 @@
+﻿using LiveHackDb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LiveHack_Web.Models.Viewmodels
+{
+	public class SponsorGroupViewModel : TeamGroupViewModel
+	{
+		public SponsorGroupViewModel(SponsorGroup group) : base(group)
+		{
+
+		}
+	}
+}
