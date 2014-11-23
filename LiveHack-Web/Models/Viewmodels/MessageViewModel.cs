3d47590f5870a55c8ce9fc68f6828251dbc8d714
Adam Bahrainwala <abahrain@purdue.edu>
2014-11-22 21:45:41 -0500

    messageviewmodel

---
+++ LiveHack-Web/Models/Viewmodels/MessageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using LiveHackDb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LiveHack_Web.Models.Viewmodels
+{
+    public class MessageViewModel
+    {
+        public MessageViewModel(Message message)
+        {
+            this.MessageId = message.MessageId;
+            this.Sender = message.Sender;
+            this.Group = message.Group;
+            this.Body = message.Body;
+            this.SendTime = message.SendTime;
+        }
+        
+        public Guid MessageId { get; set; }
+	    public User Sender { get; set; }
+	    public Group Group { get; set; }
+	    public string Body { get; set; }
+	    public DateTimeOffset SendTime { get; set; }
+    }
+}
