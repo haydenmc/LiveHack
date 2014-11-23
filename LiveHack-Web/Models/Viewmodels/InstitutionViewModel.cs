1e17cf3c6ba40523cf75ab92f3889e19da25a813
Darren Cheng <zades@outlook.com>
2014-11-22 23:23:33 -0500

    Changed all non group view models with referneces to guids

--- LiveHack-Web/Models/Viewmodels/InstitutionViewModel.cs
+++ LiveHack-Web/Models/Viewmodels/InstitutionViewModel.cs
@@ -13,13 +13,15 @@ namespace LiveHack_Web.Models.Viewmodels
             this.InstitutionId  = ins.InstitutionId;
             this.Name           = ins.Name;
             this.ZipCode        = ins.ZipCode;
-            this.Group			= new InstitutionGroupViewModel(ins.Group);
-            this.Users			= ins.Users.Select(x => new UserViewModel(x)).ToList();
+			this.GroupId		= ins.Group.GroupId;
+			//this.Group			= new InstitutionGroupViewModel(ins.Group);
+            //this.Users			= ins.Users.Select(x => new UserViewModel(x)).ToList();
 		}
         public Guid InstitutionId { get; set; }
         public string Name { get; set; }
         public string ZipCode { get; set; }
-        public InstitutionGroupViewModel Group { get; set; }
-        public ICollection<UserViewModel> Users { get; set; }
-    }
+		public Guid GroupId { get; set; }
+		//public InstitutionGroupViewModel Group { get; set; }
+		//public ICollection<UserViewModel> Users { get; set; }
+	}
 }
--- LiveHack-Web/Models/Viewmodels/UserViewModel.cs
+++ LiveHack-Web/Models/Viewmodels/UserViewModel.cs
@@ -10,15 +10,18 @@ namespace LiveHack_Web.Models.Viewmodels
     {
         public UserViewModel(User user)
         {
-            this.Id          = user.Id;
-            this.Email       = user.Email;
-            this.Hackathons   = user.Hackathons.Select(x => new HackathonViewModel(x)).ToList();
-            this.Institution = new InstitutionViewModel(user.Institution);
+            this.Id				= user.Id;
+            this.Email			= user.Email;
+			this.HackathonIds	= user.Hackathons.Select(x => x.HackathonId).ToList();
+			this.InstitutionId	= user.Institution.InstitutionId;
+            //this.Hackathons   = user.Hackathons.Select(x => new HackathonViewModel(x)).ToList();
+            //this.Institution	= new InstitutionViewModel(user.Institution);
 
         }
-
-        public ICollection<HackathonViewModel> Hackathons { get; set; }
-        public InstitutionViewModel Institution { get; set; }
+		public ICollection<Guid> HackathonIds { get; set; }
+		public Guid InstitutionId { get; set; }
+        //public ICollection<HackathonViewModel> Hackathons { get; set; }
+        //public InstitutionViewModel Institution { get; set; }
         public string Id { get; set; }
         public string Email { get; set; }
     }
